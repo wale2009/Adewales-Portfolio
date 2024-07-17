34fd90b77fcb17007adda9118dfa28f17d1f358e
wale2009 <adewaleadebayo202@gmail.com>
2024-07-17 11:44:40 +0100

    change the text

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,11 +1,11 @@
 import project1 from "../assets/projects/project-1.png";
 import project2 from "../assets/projects/project-2.png";
-// import project3 from "../assets/projects/project-3.jpg";
-// import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I'm a passionate full-stack developer with a knack for crafting robust and scalable web applications. With experience in the field, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I'm a passionate Front-end developer with a knack for crafting robust and scalable web applications. With extensive experience in front-end technologies like React and Next.js, I bring a strong focus on creating exceptional user experiences. Additionally, my background in back-end technologies such as Node.js, MySQL, PostgreSQL, and MongoDB allows me to build comprehensive solutions that drive business growth.`;
 
-export const ABOUT_TEXT = `I'm a full-stack developer who's all about creating web apps that are not only efficient but also super user-friendly. My journey into coding started with me just being curious about how stuff works, and now, here I am, loving every bit of it! I've had my hands on a bunch of cool technologies like React, Next.js, Node.js, and databases like MySQL, PostgreSQL, and MongoDB. What gets me going is solving those tricky problems, especially when I'm working with a team. Outside of coding, you'll find me staying active, checking out the latest tech trends, and even lending a hand to open-source projects whenever I can.`;
+export const ABOUT_TEXT = `I'm a Front-end developer who's all about creating web apps that are not only efficient but also super user-friendly. My journey into coding started with me just being curious about how stuff works, and now, here I am, loving every bit of it! I've had my hands on a bunch of cool technologies like React, Next.js, and Tailwind CSS for front-end development. Additionally, I have experience with back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB, which helps me build comprehensive solutions.
+
+What gets me going is solving those tricky problems, especially when I'm working with a team. Outside of coding, you'll find me staying active, checking out the latest tech trends, and even lending a hand to open-source projects whenever I can.`;
 
 export const PROJECTS = [
   {
@@ -15,7 +15,6 @@ export const PROJECTS = [
       "A fully functional weather application that shows the exact weather forecast for that particular day and seven days after.",
     technologies: [ "TailwindCSS", "TypeScript", "Node.js"],
     link: "https://weather-app-two-ivory-24.vercel.app/",
-    // add the link of the website so it can be visible when the see ur portfolio page
   },
   {
     title: "Restaurant Website",
@@ -25,20 +24,6 @@ export const PROJECTS = [
     technologies: [ "TailwindCSS", "React"],
     link: "https://w-food-2.vercel.app/",
   },
-  // {
-  //   title: "Portfolio Website",
-  //   image: project3,
-  //   description:
-  //     "A personal portfolio website showcasing projects, skills, and contact information.",
-  //   technologies: ["HTML", "CSS", "React", "Bootstrap"],
-  // },
-  // {
-  //   title: "Blogging Platform",
-  //   image: project4,
-  //   description:
-  //     "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-  //   technologies: ["HTML", "CSS", "Express", "mySQL"],
-  // },
 ];
 
 export const CONTACT = {
